9dd2d0c5cfc59a3c22963aaaa10215af49973e02
NuMetriq <info@numetriq.org>
2026-07-15 08:55:16 -0500

    Implement basic DynamicArray operations

--- src/main/java/com/kentbiler/datastructures/list/DynamicArray.java
+++ src/main/java/com/kentbiler/datastructures/list/DynamicArray.java
@@ -1,4 +1,42 @@
 package com.kentbiler.datastructures.list;
 
-public class DynamicArray {
+public class DynamicArray<T> {
+
+    private Object[] elements;
+    private int size;
+
+    public DynamicArray() {
+        elements = new Object[10];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return elements.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T value) {
+        elements[size] = value;
+        size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                "Index: " + index + ", Size: " + size
+            );
+        }
+    }
 }
