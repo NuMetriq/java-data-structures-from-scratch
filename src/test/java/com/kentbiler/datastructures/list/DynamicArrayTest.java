42e52715c9f40e2d049137901c1dc1f4bfd87029
NuMetriq <info@numetriq.org>
2026-07-16 12:07:10 -0500

    Test DynamicArray remove operation

--- src/test/java/com/kentbiler/datastructures/list/DynamicArrayTest.java
+++ src/test/java/com/kentbiler/datastructures/list/DynamicArrayTest.java
@@ -47,4 +47,19 @@ class DynamicArrayTest {
         assertEquals("Veatch", array.get(0));
         assertEquals(1, array.size());
     }
+
+    @Test
+    void removeReturnsElementAndShiftsLaterElementsLeft() {
+        DynamicArray<String> array = new DynamicArray<>();
+
+        array.add("Aristotle");
+        array.add("Menger");
+        array.add("Veatch");
+
+        String removedValue = array.remove(1);
+
+        assertEquals("Menger", removedValue);
+        assertEquals("Veatch", array.get(1));
+        assertEquals(2, array.size());
+    }
 }
