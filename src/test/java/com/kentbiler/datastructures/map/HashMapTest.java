5124839fcbdfec6f81686e5f6c784a0054f86a65
NuMetriq <info@numetriq.org>
2026-08-07 21:03:05 -0500

    Test HashMap collision handling

--- src/test/java/com/kentbiler/datastructures/map/HashMapTest.java
+++ src/test/java/com/kentbiler/datastructures/map/HashMapTest.java
@@ -7,6 +7,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HashMapTest {
 
+    private static class CollisionKey {
+        private final String value;
+
+        private CollisionKey(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public int hashCode() {
+            return 42;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (!(obj instanceof CollisionKey other)) {
+                return false;
+            }
+
+            return value.equals(other.value);
+        }
+    }
+
     @Test
     void newMapIsEmpty() {
         HashMap<String, Integer> map = new HashMap<>();
@@ -34,5 +60,20 @@ class HashMapTest {
 
         assertEquals(1, map.size());
         assertEquals(2, map.get("Aristotle"));
+    }
+
+    @Test
+    void handlesHashCollisions() {
+        HashMap<CollisionKey, Integer> map = new HashMap<>();
+
+        CollisionKey first = new CollisionKey("Aristotle");
+        CollisionKey second = new CollisionKey("Menger");
+
+        map.put(first, 1);
+        map.put(second, 2);
+
+        assertEquals(2, map.size());
+        assertEquals(1, map.get(first));
+        assertEquals(2, map.get(second));
     }
 }
